118c9d6d5c90a08f765cdefa6ddc48e38f364c43
Varun Singh Chhonker <31241955+varunchhonker@users.noreply.github.com>
2021-12-06 16:35:48 +0530

    Update excel.js

--- excel.js
+++ excel.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "http://localhost/SAC/sacexcel/xlsx.js";
+            let xlsxjs = "https://raw.githubusercontent.com/varunchhonker/SACFileUploader/main/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
